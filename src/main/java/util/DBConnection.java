3c1549a7ec3c83e9a72d00f7060e8b84ca265303
Yasmin Kimura <161663934+kimurinhakikii@users.noreply.github.com>
2025-05-19 20:21:09 -0300

    Trocando informações em DBConnection
    Tirando infos pessoais

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/SellOutEasyTrack_SQL";
-    private static final String USER = "kevin";
-    private static final String PASSWORD = "senhaforte123";
+    private static final String URL = "jdbc:mysql://localhost:3306/seubanco";
+    private static final String USER = "seu_usuario";
+    private static final String PASSWORD = "sua_senha";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
